25f36e51cce9719b378e01de4622e3f967ef7e36
rgw3d <rgw3d@aol.com>
2015-09-21 13:45:09 -0500

    Moving from grunt-sass to grunt-contrib-sass because of errors with grunt-sass

--- grunt.js
+++ grunt.js
@@ -120,7 +120,7 @@ module.exports = function(grunt) {
       }
     },
     sass: {
-      build: {
+      dist: {
         files: {
           'build/temp/video-js.css': 'src/css/video-js.scss'
         }
